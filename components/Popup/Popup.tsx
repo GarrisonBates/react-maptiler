3affae8b0b832697bfc54a45d204af252afdd888
357Labs <garrisonbates02@gmail.com>
2024-07-29 08:30:41 -0400

    Added docs to <Marker>

--- components/Popup/Popup.tsx
+++ components/Popup/Popup.tsx
@@ -27,10 +27,17 @@ type PropTypes = {
 
 /**
  * Represents a Popup on the map, which is typically (but not necessarily) associated with a Marker and usually contains text or HTML content.
- * @param children - JSX content that should be displayed within the popup
- * @param className - CSS classes to apply to the popup
- * 
- * @returns 
+ * @param children - JSX or text that should be displayed within the popup.
+ * @param className - CSS classes to apply to the popup.
+ * @param lngLat - LngLatLike containing the coordinates of the popup in the format [lng, lat] or {"lon": lng, "lat": lat}. If a lngLat prop is not passed, it will default to the lngLat of the parent <Marker> (if one exists).
+ * @param closeButton - Whether or not to display a button in the corner of the popup that can be used to close it.
+ * @param closeOnClick - Whether the popup should close when the map is clicked.
+ * @param closeOnMove - Whether the popup should close when the map is moved.
+ * @param focusAfterOpen - Whether the popup should focus itself after it has been opened.
+ * @param anchor - A string indicating the part of the Popup that should be positioned closest to the coordinate specified in lngLat. Options are 'center', 'top', 'bottom', 'left', 'right', 'top-left', 'top-right', 'bottom-left', and 'bottom-right'. If unspecified, anchor will be dynamically set to ensure the popup falls within the map container with a preference for 'bottom'.
+ * @param offset - A pixel offset applied to the popup's location. Can be specified as a number, PointLike, or object of Points specifying an offset for each anchor position (where negative offsets indicate left and up).
+ * @param maxWidth - The maximum width of the popup in pixels.
+ * @returns
  */
 export const Popup = ({
   children: innerHTML,
@@ -40,7 +47,7 @@ export const Popup = ({
   closeOnClick = true,
   closeOnMove = true,
   focusAfterOpen = true,
-  anchor = "bottom",
+  anchor,
   offset = 40,
   maxWidth,
 }: PropTypes) => {
